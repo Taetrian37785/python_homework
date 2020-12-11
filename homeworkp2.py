4d07d6cbe0fd44f95d120fed8cc724387a5e18b2
unknown <tarmonkey178@aim.com>
2020-12-11 16:16:48 -0500

    Updates the .py files as well as added the .csvs.

---
+++ homeworkp2.py
@@ -0,0 +1,48 @@
+import csv
+import os
+csv_path = os.path.join("election_data.csv")
+total_votes = 0
+candidate_options = []
+candidate_votes = {}
+winning_candidate = ""
+winning_count = 0
+with open(csv_path) as csvfile:
+    reader = csv.reader(csvfile)
+    header = next(reader)
+    txt_path = os.path.join("election results.txt")
+    textfile = open(txt_path, 'w+', newline="")
+    for row in reader:
+        total_votes = total_votes + 1
+        candidate_name = row[2]
+        if candidate_name not in candidate_options:
+            candidate_options.append(candidate_name)
+            candidate_votes[candidate_name] = 0
+        candidate_votes[candidate_name] = candidate_votes[candidate_name] + 1
+    election_results = (
+        f"\n\nElection Results\n"
+        f"-------------------------\n"
+        f"Total Votes: {total_votes}\n"
+        f"-------------------------\n")
+    textfile.write(election_results)
+    print(election_results, end="")
+    for candidate in candidate_votes:
+        votes = candidate_votes.get(candidate)
+        vote_percentage = float(votes) / float(total_votes) * 100
+        if (votes > winning_count):
+            winning_count = votes
+            winning_candidate = candidate
+        voter_output = f"{candidate}: {vote_percentage:.3f}% ({votes})\n"
+        textfile.write(voter_output)
+        print(voter_output, end="")
+    winning_candidate_summary = (
+        f"-------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"-------------------------\n")
+    textfile.write(winning_candidate_summary)
+    print(winning_candidate_summary, end="")
+
+
+    
+   
+    
+    textfile.close()
--- python_homewprk.py
+++ python_homewprk.py
@@ -1,16 +1,69 @@
 import os
 import csv
-
-cvs_path = os.path.join("..", "budget_data.csv")
+total_months= 0
+total_net = 0
+month_of_change= []  
+net_change_list= []
+greatest_increase = ["",0]
+greatest_decrease = ["",99999999999]
+cvs_path = os.path.join("budget_data.csv")
 with open(cvs_path) as csvfile:
     csvreader = csv.reader(csvfile)
 
-    print(csvreader)
+    #print(csvreader)
 
 
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
-    
+    txt_path = os.path.join("Budget Information.txt")
+    textfile = open(txt_path, 'w+', newline="")
+   # print(f"CSV Header: {csv_header}")
+    first_row = next(csvreader)
+    total_months += 1
+    total_net += int(first_row[1])
+    prev_net = int(first_row[1])
+
+    #minVal, maxVal = [], []
 
     for row in csvreader:
-         print(row)
+        total_months +=1
+        total_net += int(row[1])
+
+        net_change = int(row[1]) - prev_net
+        prev_net = int(row[1])
+        net_change_list += [net_change]
+        month_of_change += [row[0]]
+
+        if net_change > greatest_increase[1]:
+            greatest_increase[0] = row[0]
+            greatest_increase[1] = net_change
+
+        if net_change < greatest_decrease[1]:
+            greatest_decrease[0] = row[0]
+            greatest_decrease[1] = net_change
+
+net_monthly_avg = sum(net_change_list) / len(net_change_list)
+output = (
+    f"Financial Analysis \n"
+    f"--------------------\n"
+    f"Total Months: {total_months}\n"
+    f"Total: ${total_net}\n"
+    f"Average Change:$ {net_monthly_avg:.2f}\n"
+    f"Greatest Incease in Profits: {greatest_increase[0]}(${greatest_increase[1]})\n"
+    f"Greatest Decrease in Profits: {greatest_decrease[0]}(${greatest_decrease[1]})\n"
+  )
+textfile.write(output)
+print(output)
+textfile.close()
+
+  
+
+    
+    
+
+
+
+
+
+
+
+  
